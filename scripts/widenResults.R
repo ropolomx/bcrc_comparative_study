060f7d6e828c95829326e7169a793b3a2251a9ac
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-12-07 13:29:53 -0700

    Remove unnecessary data reading code

--- scripts/widenResults.R
+++ scripts/widenResults.R
@@ -90,31 +90,3 @@ amrAnalytical <- amrAnalytical %>%
   as.matrix(.)
 
 row.names(amrAnalytical) <- amrClassification
-
-# Let's now read all the Coverage Sampler tabular files
-# We are using the readr package (read_tsv)
-# We are also using the list of sample names extracted in the previous function
-# to set the names of the list elements
-# This will make life so much easier!
-
-# Join all the datasets into one dataframe that will be analyzed
-
-amrResults <- do.call("rbind", amrResults)
-
-# Need to add a column with sample name to the Coverage Sampler output
-# Also need to add a column with sample depth
-
-amrResults$SampleName <- row.names(amrResults)
-amrResults$SampleID <- str_extract(amrResults$SampleName, "^.*_rarefied")
-amrResults$SampleID <- str_replace(amrResults$SampleID, "_rarefied", "")
-amrResults$Depth <- str_extract(amrResults$SampleName, "rarefied_.*_")
-amrResults$Depth <- str_replace(amrResults$Depth, "rarefied_", "")
-amrResults$Depth <- str_replace(amrResults$Depth, "_","")
-amrResults$amrLevel <- str_extract(amrResults$SampleName, "(class|mechanism|group|gene)")
-
-# The other (easier) option is to read csv file containing all rarefied datasets which were concatenated with Python-Pandas
-
-amrRarefiedConcat <- read_csv('~/amr/2-4-8_results/2_4_8_study_RZ/amrResults_Aug2017_75_gene_frac/rarefiedConcat.csv')
-
-# The dataframes amrResults or amrRarefiedConcat that were generated above can be used for 
-#plotting rarefaction curves with ggplot2
