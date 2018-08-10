53f86d0a780bf2e70cf78c21d21d3642f091d766
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2018-08-09 19:33:00 -0600

    Kraken heatmaps accomplished

--- scripts/custom_heatmaps.R
+++ scripts/custom_heatmaps.R
@@ -6,6 +6,8 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(stringr)
+library(purrr)
+library(here)
 #source('scripts/meg_utility_functions.R')
 
 # Read data ---------------------------------------------------------------
@@ -16,17 +18,35 @@ metadata <- read.csv('BCRC_metadata.csv')
 
 amr_group_norm <- read.csv('amr_matrices/normalized/AMR_Group_Normalized.csv')
 
-krakenNorm <- read.csv('kraken_taxonReads_matrices_updated/normalized/kraken_Genus_Normalized.csv')
+tax_ranks <- c(
+  "Phylum",
+  "Class",
+  "Order",
+  "Family",
+  "Genus",
+  "Species"
+)
 
-krakenTaxonParsed <- read.csv('aggregated_data_for_analysis/krakenAnalytical_taxonReads.csv') 
+krakenNorm <-
+  tax_ranks %>%
+  map(~ read.csv(
+    here(
+      'kraken_cladeReads_matrices_updated',
+      'normalized',
+      paste0('kraken_', .x, '_Normalized.csv')
+    )
+  )) %>%
+  set_names(nm = tax_ranks)
 
-krakenTaxonParsed <- 
-  krakenTaxonParsed %>%
+krakenCladeParsed <- read.csv('aggregated_data_for_analysis/krakenAnalytical_cladeReads.csv') 
+
+krakenCladeParsed <- 
+  krakenCladeParsed %>%
   rename(id = Lineage)
 
-krakenTaxonParsed <- 
-  krakenTaxonParsed %>%
-  left_join(.,kraken_tax_dt_taxon, by = "id")
+krakenCladeParsed <- 
+  krakenCladeParsed %>%
+  left_join(.,kraken_tax_dt_clade, by = "id")
   
 
 # AMR heatmaps ------------------------------------------------------------
@@ -214,52 +234,205 @@ ggsave(
 
 # Kraken heatmap ----------------------------------------------------------
 
-krakenTax <- krakenTaxonParsed[, -(2:36)]
+krakenClade <- krakenCladeParsed[, -(2:36)]
 
-krakenNormTidy <- gather(krakenNorm, ID, counts, 2:36)
+krakenNormTidy <- 
+  krakenNorm %>%
+  map(
+    ~ gather(.x, ID, counts, 2:36)
+  )
 
-krakenNormMerge <- merge(krakenTax, krakenNormTidy, by = "Genus")
+krakenNormTidy <-
+  krakenNormTidy %>%
+  map(
+    ~ mutate(.x, ID = str_replace(ID, "FC_Con_V055", "FC_V055"))
+  )
 
-krakenNormMerge <- merge(krakenNormMerge, metadata, by = "ID")
+krakenNormMerge <- 
+  krakenNormTidy %>%
+  imap(
+    ~ merge(krakenClade, .x, by = .y)
+  )
 
-krakenNormMerge <- krakenNormMerge[krakenNormMerge$Type != "Wetlands", ]
+krakenNormMerge <- 
+  krakenNormMerge %>%
+  map(
+    ~ merge(.x, metadata, by = "ID")
+  )
 
 krakenNormMerge <-
   krakenNormMerge %>% 
-  arrange(Phylum) %>% 
-  filter(Domain != "Viruses") %>% 
-  filter(Phylum != "") %>% 
-  drop_na(Phylum)
+  map(
+    ~ arrange(.x, Phylum) %>% 
+      filter(., Domain != "Viruses") %>% 
+      filter(., Phylum != "NA")
+  )
 
-names(krakenNormMerge) <-
-  str_replace(names(krakenNormMerge), "counts", "Normalized_Counts")
+krakenNormMerge <-
+  krakenNormMerge %>%
+  map(
+    ~ rename(.x, Normalized_Counts = counts)
+  )
 
 # Re-order factors of Type column
 
-krakenNormMerge$Type <-
-  str_replace(krakenNormMerge$Type, "\\.", " ")
-
-krakenNormMerge$Type <-
-  factor(
-    krakenNormMerge$Type,
-    levels = c('Fecal Composite', 'Catch Basin', 'Soil', 'Sewage Treatment')
+krakenNormMerge <-
+  krakenNormMerge %>%
+  map(
+    ~ mutate(.x, Matrix_Type = str_replace(Matrix_Type, "_", " "))
   )
 
-krakenNormTop <- 
+krakenNormMerge <-
   krakenNormMerge %>%
-  group_by(Genus) %>%
-  summarise(Count_sum = sum(Normalized_Counts)) %>%
-  arrange(-Count_sum) %>%
-  slice(1:100)
+  map(~ mutate(.x, Matrix_Type = factor(
+    Matrix_Type,
+    levels = c('Fecal Composite', 'Catch Basin', 'Soil', 'Wastewater')
+  )))
+
+krakenNormTop_Phylum <- 
+    krakenNormMerge$Phylum %>%
+    group_by(Phylum) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Phylum != "NA") %>%
+    slice(1:100)
+
+krakenNormTop_Class <- 
+    krakenNormMerge$Class %>%
+    group_by(Class) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Class != "NA") %>%
+    slice(1:100)
+
+krakenNormTop_Order <- 
+    krakenNormMerge$Order %>%
+    group_by(Order) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Order != "NA") %>%
+    slice(1:100)
+
+krakenNormTop_Family <- 
+    krakenNormMerge$Family %>%
+    group_by(Family) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Family != "NA") %>%
+    slice(1:100)
+
+krakenNormTop_Genus <- 
+    krakenNormMerge$Genus %>%
+    group_by(Genus) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Genus != "NA") %>%
+    slice(1:100)
+
+krakenNormTop_Species <- 
+    krakenNormMerge$Species %>%
+    group_by(Species) %>%
+    summarise(Count_sum = sum(Normalized_Counts)) %>%
+    arrange(-Count_sum) %>%
+    filter(Species != "NA") %>%
+    slice(1:100)
+
+krakenNormTop <- list(
+  "Phylum" = krakenNormTop_Phylum,
+  "Class" = krakenNormTop_Class,
+  "Order" = krakenNormTop_Order,
+  "Family" = krakenNormTop_Family,
+  "Genus" = krakenNormTop_Genus,
+  "Species" = krakenNormTop_Species
+)
+
+krakenNormSubset_Phylum <- krakenNormMerge$Phylum[krakenNormMerge$Phylum$Phylum %in% krakenNormTop$Phylum$Phylum,]
+krakenNormSubset_Class <- krakenNormMerge$Class[krakenNormMerge$Class$Class %in% krakenNormTop$Class$Class,]
+krakenNormSubset_Order <- krakenNormMerge$Order[krakenNormMerge$Order$Order %in% krakenNormTop$Order$Order,]
+krakenNormSubset_Family <- krakenNormMerge$Family[krakenNormMerge$Family$Family %in% krakenNormTop$Family$Family,]
+krakenNormSubset_Genus <- krakenNormMerge$Genus[krakenNormMerge$Genus$Genus %in% krakenNormTop$Genus$Genus,]
+krakenNormSubset_Species <- krakenNormMerge$Species[krakenNormMerge$Species$Species %in% krakenNormTop$Species$Species,]
+
+krakenNormSubset <- list(
+    "Phylum" = krakenNormSubset_Phylum,
+    "Class" = krakenNormSubset_Class,
+    "Order" = krakenNormSubset_Order,
+    "Family" = krakenNormSubset_Family,
+    "Genus" = krakenNormSubset_Genus,
+    "Species" = krakenNormSubset_Species
+)
+  # krakenNormMerge[krakenNormMerge$Family %in% krakenNormTop$Family, ]
 
 krakenNormSubset <-
-  krakenNormMerge[krakenNormMerge$Genus %in% krakenNormTop$Genus, ]
+  krakenNormSubset %>%
+  map(~ mutate(.x, Matrix_Type = factor(
+    .x$Matrix_Type,
+    levels = c('Fecal Composite',
+      'Catch Basin',
+      'Soil',
+      'Wastewater')
+  )))
 
-kraken_genus_by_phylum_hm <-
-  ggplot(amr_group_subset, aes(x = ID, y = Group)) +
+krakenNormSubset <-
+  krakenNormSubset %>%
+  map(~ mutate(.x, ID = factor(
+    .x$ID,
+    levels = c(
+      "FC_A062",
+      "FC_A070",
+      "FC_S034",
+      "FC_S040",
+      "FC_N003",
+      "FC_N013",
+      "FC_V046",
+      "FC_V053",
+      "FC_V055",
+      "FC_Nat_V042",
+      "FC_Nat_V045",
+      "FC_Nat_V052",
+      "CB_A018",
+      "CB_A038",
+      "CB_S008",
+      "CB_S026",
+      "CB_S043",
+      "CB_N001",
+      "CB_N002",
+      "CB_N021",
+      "CB_N022",
+      "CB_V011",
+      "CB_V027",
+      "CB_V012",
+      "CB_V028",
+      "Soil_N_WF_02May14",
+      "Soil_N_WF_20May15",
+      "Soil_N_EF_02May14",
+      "Soil_N_EF_20May15",
+      "ST_C039",
+      "ST_C053",
+      "ST_C062",
+      "ST_M029",
+      "ST_M050",
+      "ST_M060"          
+    )
+  )))
+
+krakenNormSubset <-
+  krakenNormSubset %>%
+  map(
+    ~ .x %>%
+      mutate(.,Phylum = str_replace(Phylum, "^p_(.*)$", "\\1")) %>%
+      mutate(.,Class = str_replace(Class, "^c_(.*)$", "\\1")) %>%
+      mutate(.,Order = str_replace(Order, "^o_(.*)$", "\\1")) %>%
+      mutate(.,Family = str_replace(Family, "^f_(.*)$", "\\1")) %>%
+      mutate(.,Genus = str_replace(Genus, "^g_(.*)$", "\\1")) %>%
+      mutate(.,Species = str_replace(Species, "^s_(.*)$", "\\1"))
+  )
+
+kraken_hm <- function(kraken_subset, tax){
+  ggplot(kraken_subset, aes_string(x = 'ID', y = tax)) +
   geom_tile(aes(fill = log2(Normalized_Counts + 1))) +
   facet_grid(
-    Class ~ Type,
+    Phylum ~ Matrix_Type,
     scales = 'free',
     switch = 'x',
     drop = TRUE,
@@ -276,15 +449,15 @@ kraken_genus_by_phylum_hm <-
     #strip.text.y=element_blank(),
     #strip.background= element_rect(fill = amr80NormMerge$Class),
     #axis.text.y=element_text(size=3),
-    axis.text.x = element_blank(),
-    # axis.text.x = element_text(angle = 90,hjust=1),
+    # axis.text.x = element_blank(),
+    axis.text.x = element_text(size = 10, angle = 90,hjust=1),
     #axis.text.y=element_blank(),
-    axis.text.y = element_text(size = 7),
+    axis.text.y = element_text(size = 9),
     axis.title.x = element_text(size = 17),
     axis.title.y = element_blank(),
     legend.position = "bottom",
     legend.title = element_text(size = ),
-    panel.spacing.x = unit(0.01, "lines"),
+    panel.spacing.x = unit(0.00, "lines"),
     panel.spacing.y = unit(0.06, "lines"),
     plot.title = element_text(size = 24, hjust = 0.5),
     plot.margin = unit(c(0, 0, 0, 0), "cm")
@@ -292,17 +465,52 @@ kraken_genus_by_phylum_hm <-
   xlab('\nSample Matrix Type') +
   scale_fill_gradient(low = "black", high = "cyan") +
   labs(fill = 'Log2 Normalized Count') +
-  ggtitle(paste(
-    'Normalized AMR Group Counts',
-    ' by Class',
-    '\n',
-    sep = '',
-    collapse = ''
+    ggtitle(paste(
+      'Normalized',
+      tax,
+      'Counts',
+      'by',
+      'Phylum',
+      '\n',
+      sep = ' ',
+      collapse = ''
+    ))
+}
+
+kraken_hm_species <- kraken_hm(krakenNormSubset$Species, "Species")
+
+ggsave(
+  here(
+    'graphs_updated',
+    'Microbiome_cladeReads',
+    'kraken_top_Species_by_Phylum_hm.png'
+  ),
+  kraken_hm_species,
+  width = 18,
+  height = 14.5,
+  units = "in",
+  dpi = 600
+)
+
+all_kraken_hm <- 
+  krakenNormSubset %>%
+  imap(
+    ~ kraken_hm(.x,.y)
+  )
+
+all_kraken_hm %>%
+  iwalk( ~ ggsave(
+    filename = here(
+      'graphs_updated',
+      'Microbiome_cladeReads',
+      paste0('kraken_top_', .y, '_by_Phylum_hm.png')
+    ),
+    plot = .x,
+    width = 18,
+    height = 14.5,
+    units = "in",
+    dpi = 600
   ))
-
-
-
-
 
 
 # Fancier Kraken heatmaps -------------------------------------------------
